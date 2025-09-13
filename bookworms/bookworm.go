4d4d121dc68a602fb08d2f6469e750cb32ffbbb9
clovisphere <clovis.mugaruka@gmail.com>
2025-09-13 14:16:14 +0300

    Update business logic

--- bookworms/bookworm.go
+++ bookworms/bookworm.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"os"
+	"sort"
 )
 
 // A Bookworm contains the list of books on a bookworn's shelf.
@@ -15,6 +16,56 @@ type Bookworm struct {
 type Book struct {
 	Author string `json:"author"`
 	Title  string `json:"title"`
+}
+
+// byAuthor is a list of Book.
+// Defining a custom type to implement sort.Interface
+type byAuthor []Book
+
+// Len implements sort.Interface by
+// returning the length of the book per author.
+func (b byAuthor) Len() int { return len(b) }
+
+// Swap implements sort.Interface and swaps two books.
+func (b byAuthor) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}
+
+// Less implements sort.Interface and
+// returns books sorted by Author then Title.
+func (b byAuthor) Less(i, j int) bool {
+	if b[i].Author != b[j].Author {
+		return b[i].Author < b[j].Author
+	}
+	return b[i].Title < b[j].Title
+}
+
+// booksCount registers all the books and their occurrences
+// from the bookworms shelves.
+func booksCount(bookworms []Bookworm) map[Book]uint {
+	count := make(map[Book]uint)
+
+	for _, bookworm := range bookworms {
+		for _, book := range bookworm.Books {
+			count[book]++
+		}
+	}
+
+	return count
+}
+
+// findCommonBooks returns books are on more than one bookworm's shelf.
+func findCommonBooks(bookworms []Bookworm) []Book {
+	booksOnShelves := booksCount(bookworms)
+
+	var commonBooks []Book
+	for book, count := range booksOnShelves {
+		if count > 1 {
+			commonBooks = append(commonBooks, book)
+		}
+	}
+
+	return sortBooks(commonBooks)
 }
 
 // loadBookworms reads the file and returns the list of bookworms,
@@ -32,5 +83,12 @@ func loadBookworms(filePath string) ([]Bookworm, error) {
 		return nil, err
 	}
 
-	return nil, nil
+	return bookworms, nil
+}
+
+// sortBooks sorts the books by Author and then Title
+// in alphabetical order.
+func sortBooks(books []Book) []Book {
+	sort.Sort(byAuthor(books))
+	return books
 }
--- bookworms/main.go
+++ bookworms/main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var filePath string
+	flag.StringVar(&filePath, "path", "testdata/bookworms.json", "Path to the JSON file containing Bookworms data")
+	flag.Parse()
+
+	bookworms, err := loadBookworms(filePath)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "failed to load bookworms: %s\n", err)
+		os.Exit(1)
+	}
+
+	// pretty print `bookworms`
+	// b, _ := json.MarshalIndent(bookworms, "", " ")
+	// fmt.Println(string(b))
+
+	commonBooks := findCommonBooks(bookworms)
+
+	fmt.Println("\nHere are the common books:")
+	displayBooks(commonBooks)
+}
+
+// displayBooks prints out the titles and authors of a list of books.
+func displayBooks(books []Book) {
+	for _, book := range books {
+		fmt.Printf(" - %s by %s\n", book.Title, book.Author)
+	}
+}
