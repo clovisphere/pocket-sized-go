ebe1f30dc166f19308f2bd172edf176c9a701c6f
clovisphere <clovis.mugaruka@gmail.com>
2025-09-20 20:57:32 +0300

    Add tests for the ask and validateGuess methods

---
+++ gordle/gordle/game_internal_test.go
@@ -0,0 +1,71 @@
+package gordle
+
+import (
+	"errors"
+	"slices"
+	"strings"
+	"testing"
+)
+
+func TestGameAsk(t *testing.T) {
+	tt := map[string]struct {
+		input string
+		want  []rune
+	}{
+		"5 charcaters in english": {
+			input: "HELLO",
+			want:  []rune("HELLO"),
+		},
+		"5 characters in arabic": {
+			input: "مرحبا",
+			want:  []rune("مرحبا"),
+		},
+		"5 characters in japanese": {
+			input: "こんにちは",
+			want:  []rune("こんにちは"),
+		},
+		"3 characters in japanese": {
+			input: "こんに\nこんにちは",
+			want:  []rune("こんにちは"),
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			g := New(strings.NewReader(tc.input))
+
+			got := g.ask()
+
+			if !slices.Equal(got, tc.want) {
+				t.Errorf("expected %v, got %v", string(tc.want), string(got))
+			}
+		})
+	}
+}
+
+func TestGameValidateGuess(t *testing.T) {
+	tt := map[string]struct {
+		word     []rune
+		expected error
+	}{
+		"nominal": {
+			word:     []rune("GUESS"),
+			expected: nil,
+		},
+		"too long": {
+			word:     []rune("POCKET"),
+			expected: errInvalidWordLength,
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			g := New(nil)
+
+			err := g.validateGuess(tc.word)
+			if !errors.Is(err, tc.expected) {
+				t.Errorf("%c, expected %q, got %q", tc.word, tc.expected, err)
+			}
+		})
+	}
+}
